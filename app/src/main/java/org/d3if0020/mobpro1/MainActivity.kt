d6d5b1ebd9d44dbc32028c10213780e68dd28c2b
Nama <ssekaa1305@gmail.com>
2024-02-23 17:04:52 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if0020/mobpro1/MainActivity.kt
@@ -0,0 +1,48 @@
+package org.d3if0020.mobpro1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.d3if0020.mobpro1.ui.theme.Mobpro1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Mobpro1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("\n Nama : Azka Faris Akbar \n " +
+                            "Kelas : D3IF - 46 - 03 \n " +
+                            "Nim : 6706220020")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Mobpro1Theme {
+        Greeting("Android")
+    }
+}
